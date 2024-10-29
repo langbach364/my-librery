0ef5c2b9ba190b7fc262fdcf6636c765fe54c8c8
langbach364 <96160493+langbach364@users.noreply.github.com>
2024-10-29 17:27:00 +0700

    Update broadcast_tool.bash

--- broadcast_tool.bash
+++ broadcast_tool.bash
@@ -6,11 +6,9 @@ fileName="nameSocket.sock"
 $commandTool && {
     echo "Công cụ đã xử lý xong"
     echo "SUCCESS" >$fileName
-    rm -f $fileName
     exit 0
 } || {
     echo "Công cụ xử lý thất bại"
     echo "FAIL" >$fileName
-    rm -f $fileName
     exit 1
 }
